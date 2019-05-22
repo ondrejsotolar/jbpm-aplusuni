19ee9e885248096b8e92436692984a9817585430
wbadmin <>
2019-05-22 14:56:11 +0200

    {/src/main/java/com/exam/exam/AppealAge_response.java}

--- src/main/java/com/exam/exam/AppealAge_response.java
+++ src/main/java/com/exam/exam/AppealAge_response.java
@@ -25,5 +25,10 @@ public class AppealAge_response implements java.io.Serializable {
 	public AppealAge_response(java.lang.Boolean isoldenough) {
 		this.isoldenough = isoldenough;
 	}
+	
+	@Override
+	public String toString() {
+	    return "response is: " + String.valueOf(isoldenough);
+	}
 
 }
